85d5f815f4d125a26f823e7ccde33b0879a25d2a
unknown <mbuguamels@gmail.com>
2023-01-30 19:35:28 +0300

    created the skills section

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@
 import Home from './Components/Home';
 import Navbar from './Components/Navbar';
 import About from './Components/About';
+import Skills from './Components/Skills';
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
       <Navbar />
       <Home />
       <About />
+      <Skills />
 
     </div>
   );
---
+++ src/Components/Skills.js
@@ -0,0 +1,20 @@
+import React from 'react'
+import './Skills.css'
+
+
+const Skills = () => {
+  return (
+    <div className='skills'>
+        <h1>Skills</h1>
+        <p>These are the skills i Have worked with</p>
+        <div>
+
+        </div>
+        <div>
+
+        </div>
+        </div>
+  )
+}
+
+export default Skills
