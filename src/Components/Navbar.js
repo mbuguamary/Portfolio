a06c64b054cfbb02df6a44897cef5c070858465d
unknown <mbuguamels@gmail.com>
2023-01-30 17:09:10 +0300

    changes on the navbar

---
+++ src/Components/Navbar.js
@@ -0,0 +1,25 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <div className='container'>
+        <div>
+        <p className='name'>Mary Mbugua.</p>
+        </div>
+        <div>
+        <nav>
+       
+     <ul>
+        <li>Home</li>
+        <li>About</li>
+        <li>Services</li>
+        <li>Portfolio</li>
+        <li>Contact</li>
+     </ul>
+     </nav>
+     </div>
+        </div>
+  )
+}
+
+export default Navbar
