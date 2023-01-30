8d3fdafed188eb79838bae4b9d82c218bfe38737
unknown <mbuguamels@gmail.com>
2023-01-30 19:16:18 +0300

    added text for about description

--- src/Components/About.js
+++ src/Components/About.js
@@ -12,6 +12,10 @@ const About = () => {
                 </div>
                 <div className='row-2'></div>
                 <h1 className='sub-title'>About Me</h1>
+                <p className='txt'>I am passionate about building excellent software that improves
+            the lives of those around me. I specialize in creating software
+            for clients ranging from individuals and small-businesses all the
+            way to large enterprise corporations. Looking forward to interact in web development Projects?</p>
             </div>
         </div>
     </div>
