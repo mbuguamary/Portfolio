90acbfc77f13b1888916b9295a928e9e9c770ae6
unknown <mbuguamels@gmail.com>
2023-01-30 18:14:17 +0300

    creating divs for the about section

---
+++ src/Components/About.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const About = () => {
+  return (
+    <div id='about'>
+        <div className='container'>
+            <div className='row'>
+                <div className='row-1'></div>
+                <div className='row-2'></div>
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default About
