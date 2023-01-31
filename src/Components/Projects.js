f50860d28ba6d817bd13601427029101ad4101c9
unknown <mbuguamels@gmail.com>
2023-01-31 10:00:31 +0300

    added social icons

--- src/Components/Projects.js
+++ src/Components/Projects.js
@@ -1,4 +1,12 @@
 import React from 'react'
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faYoutube,
+  faFacebook,
+  faTwitter,
+  faInstagram,
+  faGithub
+} from "@fortawesome/free-brands-svg-icons";
 
 const Projects = () => {
   return (
@@ -13,8 +21,29 @@ const Projects = () => {
        <div>
 
        </div>
-       <div>Social Icons</div>
-       <div><button>Download Resume</button></div>
+       <div className='social-icons'>
+        <h5>Social Icons</h5>
+        <ul>
+            
+            <a href="https://www.youtube.com/c/jamesqquick"
+        className="youtube social">
+        <FontAwesomeIcon icon={faGithub} size="2x" />
+      </a>
+      <a href="https://www.facebook.com/learnbuildteach/"
+        className="facebook social">
+        <FontAwesomeIcon icon={faFacebook} size="2x" />
+      </a>
+      <a href="https://www.twitter.com/jamesqquick" className="twitter social">
+        <FontAwesomeIcon icon={faTwitter} size="2x" />
+      </a>
+      <a href="https://www.instagram.com/learnbuildteach"
+        className="instagram social">
+        <FontAwesomeIcon icon={faInstagram} size="2x" />
+      </a>
+        </ul>
+
+       </div>
+       {/* <div><button > <a href='Images/CV.pdf' download={Images/CV.pdf}></a>Download Resume</button></div> */}
   </div>
   )
 }
