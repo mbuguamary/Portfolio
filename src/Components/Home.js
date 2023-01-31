d0bec892e735fc3de45d14f9aadb187898eff8e1
unknown <mbuguamels@gmail.com>
2023-01-31 12:04:14 +0300

    styling the home image

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -1,13 +1,17 @@
 import React from 'react'
+import mary from '../Images/mary.png'
 
 const Home = () => {
   return (
     <div className='home'>
-      <div>
+      <div className= 'rows-1'>
         <h1>Hi, I,m <span>Mary</span></h1>
         <h1>Mbugua From Kenya</h1>
         <p>Full Stack Developer</p>
         </div>
+        <div className='rows-2'>
+        <img src={mary} alt='img'/>
+       </div>
         
     </div>
   )
