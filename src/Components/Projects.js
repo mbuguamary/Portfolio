a129620cb44f33760ca803e4f689b7bd8ced71ca
unknown <mbuguamels@gmail.com>
2023-01-31 10:36:39 +0300

    styles for social icons

--- src/Components/Projects.js
+++ src/Components/Projects.js
@@ -1,12 +1,13 @@
 import React from 'react'
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import {
-  faYoutube,
+  faLinkedin,
   faFacebook,
   faTwitter,
   faInstagram,
   faGithub
 } from "@fortawesome/free-brands-svg-icons";
+import './Project.css';
 
 const Projects = () => {
   return (
@@ -22,21 +23,24 @@ const Projects = () => {
 
        </div>
        <div className='social-icons'>
-        <h5>Social Icons</h5>
+        <h6>Social Icons</h6>
         <ul>
             
-            <a href="https://www.youtube.com/c/jamesqquick"
-        className="youtube social">
+        <a href="https://github.com/mbuguamary"
+        className="github social">
         <FontAwesomeIcon icon={faGithub} size="2x" />
       </a>
-      <a href="https://www.facebook.com/learnbuildteach/"
+      <a href="https://www.facebook.com/mary.mbuguah.90"
         className="facebook social">
         <FontAwesomeIcon icon={faFacebook} size="2x" />
       </a>
       <a href="https://www.twitter.com/jamesqquick" className="twitter social">
         <FontAwesomeIcon icon={faTwitter} size="2x" />
       </a>
-      <a href="https://www.instagram.com/learnbuildteach"
+      <a href="https://www.linkedin.com/in/mary-w-mbugua/" className="linkened social">
+        <FontAwesomeIcon icon={faLinkedin} size="2x" />
+      </a>
+      <a href="https://www.instagram.com/mbuguahmian/"
         className="instagram social">
         <FontAwesomeIcon icon={faInstagram} size="2x" />
       </a>
