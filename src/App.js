5934362ddad2f7fc1958c422a43d1cb97be2927d
unknown <mbuguamels@gmail.com>
2023-02-03 12:51:32 +0300

    contact section

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import About from './Components/About';
 import Skills from './Components/Skills';
 import Projects from './Components/Projects';
 import Contact from './Components/Contact';
+import Footer from './Components/Footer';
 
 function App() {
   return (
@@ -17,6 +18,7 @@ function App() {
       <Skills />
       <Projects />
       <Contact />
+      <Footer />
 
     </div>
   );
---
+++ src/Components/Footer.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <div className='footer'>
+        <div>
+    <h5>Created by <span>Mary Mbugua</span> | 2023 All rights Reserved.</h5>
+    </div>
+    </div>
+  )
+}
+
+export default Footer
