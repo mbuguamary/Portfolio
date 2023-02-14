1b228bb09269e5d587d08ed3839028b472f14d77
Mary Mbugua <mbuguamels@gmail.com>
2023-02-14 10:56:39 +0300

    reorganized the page ,enalble download pdf feature

--- src/Components/About.js
+++ src/Components/About.js
@@ -10,21 +10,24 @@ const About = () => {
     <div id='about'>
         <div className='container'>
             <div className='row'>
-                <div className='row-1'>
-                <button className='btn1'>
-               <Document file={pdfFile}>
-               {/* <Page pageNumber={1} /> */}
-                </Document>
-                <h3>View CV</h3>
-                 </button>
-                </div>
-                <div className='row-2'></div>
+               
+               
                 <h1 className='sub-title'>About Me</h1>
                 <p className='txt'>I am passionate about building excellent software that improves
             the lives of those around me. I specialize in creating software
             for clients ranging from individuals and small-businesses all the
             way to large enterprise corporations. Looking forward to interact in web development Projects.</p>
             </div>
+            <div className='row-1'>
+                <button className='btn1'>
+              <a className='btn1' href="Mary Wairimu CV.pdf" download="Mary Wairimu CV.pdf">
+               
+              Download CV
+              </a>
+             
+               
+                 </button>
+                </div>
         </div>
     </div>
   )
