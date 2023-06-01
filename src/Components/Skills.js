7f3c0e51d2766312ae84365d01f54ae2bb54364c
Mary Mbugua <mbuguamels@gmail.com>
2023-06-01 16:16:45 +0300

    added more skills

--- src/Components/Skills.js
+++ src/Components/Skills.js
@@ -15,52 +15,53 @@ const Skills = () => {
   return (
     <div className='skills'>
         <h1>Skills</h1>
-        <p>These are the technologies i Have worked with</p>
+        <p>These are the technologies i have worked with</p>
         <div className='image-1'>
-            <div>
+            <div className='tech'>
             <img src={HTML} alt="Html"/>
             <h5>HTML</h5>
             </div>
-            <div>
+            <div className='tech'>
             <img src={CSS} alt="Css"/>
             <h5>Css</h5>
             </div>
-            <div>
+            <div className='tech'>
             <img src={JavaScript} alt="Javascript"/>
             <h5>Javascript</h5>
             </div>
-            </div>
-            <div className='image-2'>
-            <div>
+           
+            {/* <div className='image-2'> */}
+            <div className='tech'>
             <img src={ReactImg} alt="React"/>
             <h5>React</h5>
             </div>
         
         
-           <div>
+           <div className='tech'>
             <img src={JAVA} alt="Java"/>
             <h5>Java</h5>
             </div>
-            <div>
+            <div className='tech'>
             <img src={Node} alt="Node"/>
             <h5>Node</h5>
             </div>
-            </div>
-            <div className='image-3'>
-            <div>
+            {/* </div> */}
+            {/* <div className='image-3'> */}
+            <div className='tech'>
             <img src={Rails} alt="Node"/>
             <h5>Ruby on Rails</h5>
             </div>
             
-            <div>
+            <div className='tech'>
             <img src={GitHub} alt="Github"/>
             <h5>Github</h5>
             </div>
-            <div>
+            <div className='tech'>
             <img src={POSTGRESQL} alt="Postgresql"/>
             <h5>Postgresql</h5>
             </div>
-        </div>
+            </div>
+        {/* </div> */}
         </div>
   )
 }
