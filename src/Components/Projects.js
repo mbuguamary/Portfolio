15a49bfb87cf86be9a1b816f70636a0a9598b398
Mary Mbugua <mbuguamels@gmail.com>
2023-06-01 16:16:16 +0300

    added my projects

--- src/Components/Projects.js
+++ src/Components/Projects.js
@@ -4,33 +4,48 @@ import './Project.css';
 import sacco from '../Images/sacco.jpg';
 import helpdesk from '../Images/helpdesk.jpg';
 import travel from '../Images/travel.jpg';
-
+import realestate from '../Images/realestate.jpg'
+import real from '../Images/real.jpg'
 const Projects = () => {
   return (
     <div className='projects'>
        <h1>Projects</h1> 
        <div className='project-icons'>
-       <div>
+       <div className='imges'>
+        <h3>Comps Analytics</h3>
+        <img  src={realestate} alt='Real estate'/>
+        
+       <h3>This is a web application which can be used to value differnt types of properties by property values</h3>
+       <a className= 'a' href='https://comps-analytics.vercel.app/'>View Live Link</a>
+       </div>
+       <div className='imges'>
         <h3>Sacco Website</h3>
         <img  src={sacco} alt='Sacco'/>
         
-       <h3>This is a web application <br/>where sacco members can  <br/>view their diviends,apply <br/>for loans </h3>
+       <h3>This is a web application where sacco members can  view their diviends,apply for loans </h3>
+       <a className= 'a' href='https://metro-help-desk.netlify.app/'></a>
        </div>
-       <div>
+       <div className='imges'>
         <h3>Help desk App</h3>
        <img  src={helpdesk} alt='Help Desk'/>
-       <h3>This is a web application <br/>where users can raise tickets <br/>on various issues to IT<br/> Department For Help</h3>
+       <h3>This is a web application where users can raise tickets on various issues to IT Department For Help</h3>
+       <a className= 'a' href='https://metro-help-desk.netlify.app/'>View Live Link</a>
        </div>
-       <div>
+       <div className='imges'>
         <h3>Travel Website</h3>
        <img  src={travel} alt='Travel'/>
-       <h3>This is a website where <br/> users can view travel <br/>information and book for <br/>Activities</h3>
+       <h3>This is a website where  users can view travelinformation and book for Activities</h3>
+       <a className= 'a' href='https://cliiftravelwebsite.netlify.app/'>View Live Link</a>
+       </div>
+       <div className='imges'>
+       <h3>Vallies Estate</h3>
+       <img className='img' src={real} alt='real'/>
+       <h3>This is a website where  users can view houses available for purchase <br/>in the Website</h3>
+       <a className= 'a' href='https://metro-help-desk.netlify.app/'>View Live Link</a>
        </div>
        </div>
        
-       <div>
        
-       </div>
        {/* <div><button > <a href='Images/CV.pdf' download={Images/CV.pdf}></a>Download Resume</button></div> */}
  
   </div>
